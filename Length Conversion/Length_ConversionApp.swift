685346c7c95d6777e8a242956219f15f1a53047a
syrene <144994323+shb1383@users.noreply.github.com>
2024-09-19 18:05:36 +0900

    Initial Commit

---
+++ Length Conversion/Length_ConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Length_ConversionApp.swift
+//  Length Conversion
+//
+//  Created by Syrene Haber Bartolome on 2024/09/19.
+//
+
+import SwiftUI
+
+@main
+struct Length_ConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Length ConversionTests/Length_ConversionTests.swift
@@ -0,0 +1,17 @@
+//
+//  Length_ConversionTests.swift
+//  Length ConversionTests
+//
+//  Created by Syrene Haber Bartolome on 2024/09/19.
+//
+
+import Testing
+@testable import Length_Conversion
+
+struct Length_ConversionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
